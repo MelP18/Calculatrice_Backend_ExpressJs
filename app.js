16baa5b58c27d91930e485b0b6ffaab9971ba580
MelP18 <priscagnansounou99@gmail.com>
2026-02-06 16:42:19 +0100

    update

--- app.js
+++ app.js
@@ -5,37 +5,40 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var userRouter = require('./routes/user')
-var homeRouter = require('./routes/home')
+var userRouter = require('./routes/user');
+var homeRouter = require('./routes/home');
 
-require('./config/database')//config mongodb
-
+require('./config/database');
 
 var app = express();
 
-//send in tbe DB
-const bodyParser = require('body-parser');
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended : true}));
+/* =======================
+   CORS (AVANT TOUT)
+======================= */
+const corsOptions = {
+  origin: 'https://calculatricemelp.netlify.app',
+  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: false,
+};
 
+app.use(cors(corsOptions));
+app.options('*', cors(corsOptions)); // indispensable sur Vercel
+
+/* =======================
+   Middlewares
+======================= */
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-
-const corsOptions = {
-    origin: 'https://calculatricemelp.netlify.app',
-    methods: 'GET, HEAD, PUT, PATCH, POST, DELETE',
-    credentials: false,
-    optionsSuccessStatus: 204,
-  };
-  
-app.use(cors(corsOptions));
-
-
+/* =======================
+   Routes
+======================= */
 app.use('/', indexRouter);
-app.use('/auth', userRouter)
-app.use('/home', homeRouter)
+app.use('/auth', userRouter);
+app.use('/home', homeRouter);
+
 module.exports = app;
