6e20934b71f84dd04c169c696e01f911874c3985
MelP18 <priscagnansounou99@gmail.com>
2026-02-12 14:30:40 +0100

    upgrade

--- storage.js
+++ storage.js
@@ -1,14 +1,30 @@
+// const multer = require('multer')
+// const path = require('path')
+
+// const storage = multer.diskStorage({
+//     destination:(req, file, cb) =>{
+//         cb(null, 'avatars')
+//     },
+//     filename:(req, file, cb) =>{
+//         const fileName = Date.now() + path.extname(file.originalname)
+//         cb(null, fileName)
+//     }
+// })
+
+// module.exports = storage
 const multer = require('multer')
 const path = require('path')
 
 const storage = multer.diskStorage({
-    destination:(req, file, cb) =>{
+    destination: (req, file, cb) => {
         cb(null, 'avatars')
     },
-    filename:(req, file, cb) =>{
+    filename: (req, file, cb) => {
         const fileName = Date.now() + path.extname(file.originalname)
         cb(null, fileName)
     }
 })
 
-module.exports = storage
+const upload = multer({ storage })
+
+module.exports = upload
